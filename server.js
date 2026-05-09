26ee0b18b66c900b4f8972f8ef76dca553588b66
skulldracula02 <clementtshwarelo1@gmail.com>
2026-05-09 06:38:52 +0200

    Fix server.js for Vercel serverless deployment

--- server.js
+++ server.js
@@ -1,30 +1,13 @@
-// Notify endpoint to confirm Payfast payment
-app.post('/Payfast-notify', (req, res) => {
-  const data = req.body;
-  console.log('Payfast notify received:', data);
+ const express = require('express');
+const app = express();
 
-  // Verify checksum again for security
-  const params = {
-    siteCode: data.siteCode,
-    countryCode: data.countryCode,
-    currencyCode: data.currencyCode,
-    amount: data.amount,
-    transactionReference: data.transactionReference,
-    bankReference: data.bankReference,
-    customerEmail: data.customerEmail,
-    successUrl: data.successUrl,
-    cancelUrl: data.cancelUrl,
-    notifyUrl: data.notifyUrl
-  };
-  const checksum = generateChecksum(params);
+// Middleware
+app.use(express.json());
 
-  if (checksum !== data.checksum) {
-    console.error('Invalid checksum in notify');
-    return res.status(400).send('Invalid checksum');
-  }
-
-  // Mark order as paid in your database here
-  console.log(`Order ${data.transactionReference} confirmed as paid`);
-
-  res.status(200).send('OK');
+// Example test route
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Ghost Store backend is running!' });
 });
+
+// Export the app for Vercel serverless functions
+module.exports = app;
